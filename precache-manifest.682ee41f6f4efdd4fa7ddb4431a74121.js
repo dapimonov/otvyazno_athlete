25cacb6a83ae439ae29ad3c0c8b1156138f42c9a
Dmitrii Pimonov <dapimonov@gmail.com>
2020-08-18 18:40:28 +0300

    Updates

---
+++ precache-manifest.682ee41f6f4efdd4fa7ddb4431a74121.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4963fd9766f95244f06651c3cbd93084",
+    "url": "/otvyazno_athlete/index.html"
+  },
+  {
+    "revision": "91946d7c6d05d5a2390e",
+    "url": "/otvyazno_athlete/static/css/main.a75fef76.chunk.css"
+  },
+  {
+    "revision": "28fc0d3567cea6a55c46",
+    "url": "/otvyazno_athlete/static/js/2.3802c35c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/otvyazno_athlete/static/js/2.3802c35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91946d7c6d05d5a2390e",
+    "url": "/otvyazno_athlete/static/js/main.d10ba35c.chunk.js"
+  },
+  {
+    "revision": "e1d86152876ffb0e74c0",
+    "url": "/otvyazno_athlete/static/js/runtime-main.4436c72e.js"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBlack.1808b5f6.ttf"
+  },
+  {
+    "revision": "9d94b7eaedb634be56f0718ee3c0d2c2",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBlack.9d94b7ea.eot"
+  },
+  {
+    "revision": "d403436eee6af142bb056eaecb76805d",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBlack.d403436e.woff"
+  },
+  {
+    "revision": "2f1535ec92e27193cc13e35495cd845e",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBold.2f1535ec.ttf"
+  },
+  {
+    "revision": "6a54f6727e1dfd8538bbb4ab224c311f",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBold.6a54f672.woff"
+  },
+  {
+    "revision": "b6a25a9b8ae3bd903f71a761f1a6e4bd",
+    "url": "/otvyazno_athlete/static/media/MuseoSansBold.b6a25a9b.eot"
+  },
+  {
+    "revision": "3258ca8bb3a1861325c63ec376c3941a",
+    "url": "/otvyazno_athlete/static/media/MuseoSansLight.3258ca8b.ttf"
+  },
+  {
+    "revision": "e386bd26958d7169f69e43d56416a1fd",
+    "url": "/otvyazno_athlete/static/media/MuseoSansLight.e386bd26.woff"
+  },
+  {
+    "revision": "e8adc41247ec44f51d1cead8fdf45b73",
+    "url": "/otvyazno_athlete/static/media/MuseoSansLight.e8adc412.eot"
+  },
+  {
+    "revision": "2bac225e7c716a3569ae1bea77655910",
+    "url": "/otvyazno_athlete/static/media/MuseoSansMedium.2bac225e.eot"
+  },
+  {
+    "revision": "db1355d5917b9e8bbc77aff8a22b9574",
+    "url": "/otvyazno_athlete/static/media/MuseoSansMedium.db1355d5.woff"
+  },
+  {
+    "revision": "e1edf31751b631539c754a498a968d89",
+    "url": "/otvyazno_athlete/static/media/MuseoSansMedium.e1edf317.ttf"
+  },
+  {
+    "revision": "b3e4dd558860efbe20dd27e5fbb66d73",
+    "url": "/otvyazno_athlete/static/media/MuseoSansRegular.b3e4dd55.woff"
+  },
+  {
+    "revision": "ef3daa6f0cfcb6366ada64dbbaa6501d",
+    "url": "/otvyazno_athlete/static/media/MuseoSansRegular.ef3daa6f.ttf"
+  },
+  {
+    "revision": "f30df4fe1c424f83e83faae7c4a13db9",
+    "url": "/otvyazno_athlete/static/media/MuseoSansRegular.f30df4fe.eot"
+  },
+  {
+    "revision": "ca0c7be6300d44708673f475dafcf359",
+    "url": "/otvyazno_athlete/static/media/garanichev.ca0c7be6.png"
+  },
+  {
+    "revision": "3b537aa0a1e266fc4a6337d59f511ce3",
+    "url": "/otvyazno_athlete/static/media/gazprom.3b537aa0.svg"
+  },
+  {
+    "revision": "7746f27be6cd20bd7678d37beba10aca",
+    "url": "/otvyazno_athlete/static/media/medal_first.7746f27b.svg"
+  },
+  {
+    "revision": "02c73e2f2f6bd4925cc24d94a6b27da0",
+    "url": "/otvyazno_athlete/static/media/medal_pb.02c73e2f.svg"
+  },
+  {
+    "revision": "1d2c5ba560cd2ff8e48c94fca30497bb",
+    "url": "/otvyazno_athlete/static/media/medal_second.1d2c5ba5.svg"
+  },
+  {
+    "revision": "0e09d618ec7620b26e21e0b86fd53d65",
+    "url": "/otvyazno_athlete/static/media/medal_third.0e09d618.svg"
+  },
+  {
+    "revision": "dd9dee0b049f4b108ad1ba15a157e5ba",
+    "url": "/otvyazno_athlete/static/media/russia.dd9dee0b.svg"
+  }
+]);
